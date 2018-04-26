390572767b9cc6e29454a91432eb445550227298
David Velarde <davidvpe@gmail.com>
2018-04-26 20:19:14 +0200

    Project commit

---
+++ source/swiftethereum/swiftethereum.h
@@ -0,0 +1,19 @@
+//
+//  swiftethereum.h
+//  swiftethereum
+//
+//  Created by David Velarde on 4/26/18.
+//  Copyright © 2018 David Velarde. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for swiftethereum.
+FOUNDATION_EXPORT double swiftethereumVersionNumber;
+
+//! Project version string for swiftethereum.
+FOUNDATION_EXPORT const unsigned char swiftethereumVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <swiftethereum/PublicHeader.h>
+
+
